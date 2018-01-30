7c8350e65bbc3b3600824333f986c7a04fbf138c
Shohei <shohei51@gmail.com>
2018-01-30 16:14:42 +0000

    Initialized react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">The Book Trade Club</h1>
+          <h3>...needs an original name</h3>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
